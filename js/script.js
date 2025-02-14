60e3e1851487c71330494b4973aed9bbd20f4ac8
yor87 <Yorluistorres87@gmail.com>
2025-02-14 01:27:24 -0400

    Delete js directory

--- js/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("header.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("header").innerHTML = data;
-        });
-});
